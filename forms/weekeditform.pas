dce035498f8b792c4c7cd6efba8b99d413e1d080
Philip Märksch <comradephilos@googlemail.com>
2014-05-05 12:15:26 +0200

    added a new event for deleting

--- forms/weekeditform.pas
+++ forms/weekeditform.pas
@@ -12,6 +12,8 @@ uses
 
 type
 
+    TRemoveEvent = procedure(Sender: TObject; Index: Integer) of object;
+
   { TForm3 }
 
   TForm3 = class(TForm)
@@ -21,16 +23,23 @@ type
     BackButton: TBitBtn;
     Label1: TLabel;
     UndoButton: TBitBtn;
+		UndoButton1: TBitBtn;
     WeekGrid: TStringGrid;
 
     procedure BackButtonClick(Sender: TObject);
+		procedure DeleteWeek(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
     { private declarations }
     FWeek: TWorkWeek;
+    FIndex: Integer;
+
+    FOnRemoveClick: TRemoveEvent;
   public
     { public declarations }
     procedure showWeek(AWeek: TWorkWeek; ANumber: Integer);
+
+    property OnRemoveClick: TRemoveEvent read FOnRemoveClick write FOnRemoveClick;
   end;
 
 var
@@ -52,9 +61,20 @@ begin
   self.Visible := False;
 end;
 
+procedure TForm3.DeleteWeek(Sender: TObject);
+begin
+   if assigned(FOnRemoveClick) then
+   begin
+     FOnRemoveClick(self, FIndex);
+     self.Visible := False;
+   end;
+end;
+
+
 procedure TForm3.showWeek(AWeek: TWorkWeek; ANumber: Integer);
 begin
   Label1.Caption := 'Period ' + IntToStr(ANumber+1) + ' (Length: ' + IntToStr(AWeek.WeekLength) +')' ;
+  FIndex := ANumber;
   WeekDaysToStringGrid(WeekGrid, AWeek);
 end;
 
